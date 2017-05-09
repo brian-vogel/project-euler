dffc94c04078184822e8d3f8bc7511ae4766bb3b
Brian Vogel <brian.vogel123@gmail.com>
2017-05-08 20:56:58 -0600

    Solved problem 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import { problem_001 } from './problems/problem_001';
+import { problem_002 } from './problems/problem_002';
 
 console.log('problem_001:', problem_001());
+console.log('problem_002:', problem_002());
