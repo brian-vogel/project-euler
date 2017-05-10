24ca1e3be4c8c2b235de915e5665b63cd0f8f9cc
Brian Vogel <brian.vogel123@gmail.com>
2017-05-09 19:09:36 -0600

    Renamed files and created exporting index file.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { problem_001 } from './problems/problem_001';
-import { problem_002 } from './problems/problem_002';
-import { problem_003 } from './problems/problem_003';
+import * as problems from './problems/problems-index';
 
 
-console.log('problem_001:', problem_001());
-console.log('problem_002:', problem_002());
-console.log('problem_003:', problem_003());
+console.log('problem_001:', problems.problem_001());
+console.log('problem_002:', problems.problem_002());
+console.log('problem_003:', problems.problem_003());
 
---
+++ src/problems/problems-index.ts
@@ -0,0 +1,9 @@
+import { problem_001 } from './problem-001';
+import { problem_002 } from './problem-002';
+import { problem_003 } from './problem-003';
+
+export {
+  problem_001,
+  problem_002,
+  problem_003,
+}
